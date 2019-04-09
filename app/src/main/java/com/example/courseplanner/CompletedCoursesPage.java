e2b210b11ecada5c6ae77c079be0497cf1e48739
Jwheela <wheeland.jacob@hotmail.com>
2019-04-09 15:35:45 -0500

    Changed "Dude its lit" to "Bee Movie" on activity_main.xml

---
+++ app/src/main/java/com/example/courseplanner/CompletedCoursesPage.java
@@ -0,0 +1,13 @@
+package com.example.courseplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompletedCoursesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_courses_page);
+    }
+}
---
+++ app/src/main/java/com/example/courseplanner/RecommendedCoursesPage.java
@@ -0,0 +1,13 @@
+package com.example.courseplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecommendedCoursesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended_courses_page);
+    }
+}
